dbb60f7cb394bb4bdfb4e3d5dc30d21b94485f99
Deb <39284636+debyajit@users.noreply.github.com>
2023-04-20 20:32:32 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,33 +1,31 @@
-# Import the xml.etree.ElementTree module
 import xml.etree.ElementTree as ET
 
-# Define the path of the XML file
-xml_file = "C:\\Test\\employee1.xml"
+# Path to the sample DataStage job XML file
+xml_file_path = "path/to/sample/job.dsx"
 
-# Parse the XML file and get the root element
-tree = ET.parse(xml_file)
+# Parse the XML file using ElementTree
+tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
-# Define a function to get job information from an element
-def get_job_info(element):
-    # Create an empty dictionary to store job information
-    job_info = {}
-    # Loop through the attributes of the element
-    for key, value in element.attrib.items():
-        # Add the attribute name and value to the dictionary
-        job_info[key] = value
-    # Loop through the child elements of the element
-    for child in element:
-        # Add the child tag and text to the dictionary
-        job_info[child.tag] = child.text
-    # Return the dictionary
-    return job_info
+# Print basic information from the JobHeader section
+print("Job Name: ", root.find("JobHeader/JobName").text)
+print("Job Description: ", root.find("JobHeader/JobDescription").text)
+print("Job Version: ", root.find("JobHeader/Version").text)
+print("Job Creator: ", root.find("JobHeader/Creator").text)
 
-# Get the first child element of the root element, which is <Job>
-job_element = root[0]
+# Print the name and configuration of each stage in the JobDef section
+for stage in root.findall("JobDef/Stage"):
+    print("Stage Name: ", stage.find("Name").text)
+    print("Stage Type: ", stage.get("stageType"))
+    for param in stage.findall("Properties/Property"):
+        print("  ", param.get("name"), ":", param.text)
 
-# Get the job information from the job element
-job_info = get_job_info(job_element)
+# Print any parameters defined in the JobParameters section
+for param in root.findall("JobParameters/Parameter"):
+    print("Parameter Name: ", param.get("name"))
+    print("Parameter Value: ", param.text)
 
-# Print the job information
-print(job_info)
+# Print any additional properties defined in the JobProperties section
+for prop in root.findall("JobProperties/Property"):
+    print("Property Name: ", prop.get("name"))
+    print("Property Value: ", prop.text)
