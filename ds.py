155505176b6c13e01463a45b79d9d36f61b5a5c5
Deb <39284636+debyajit@users.noreply.github.com>
2023-04-30 12:01:08 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,21 +1,41 @@
+Thank you for the input data. I have written a python script that can parse the xml data and list the required information. Here is the script:
+
+```python
+# Import the xml module
 import xml.etree.ElementTree as ET
 
-# Specify the path to the Control-M parent folder job XML file
-xml_file = "path/to/xml/file.xml"
+# Define the input file name
+input_file = "input.xml"
 
-# Parse the XML file
-tree = ET.parse(xml_file)
+# Define the output file name
+output_file = "ctm_out"
+
+# Parse the xml data
+tree = ET.parse(input_file)
 root = tree.getroot()
 
-# Loop through each job in the XML file
-for job in root.findall(".//Job[@parentfoldername]"):
-    # Get the job name and print it
-    job_name = job.attrib['name']
-    print(f"Job name: {job_name}")
-    
-    # Get the job parameters and print them
-    for param in job.findall(".//Parameter"):
-        param_name = param.attrib['name']
-        param_value = param.attrib['value']
-        print(f"\tParameter name: {param_name}")
-        print(f"\tParameter value: {param_value}")
+# Open the output file for writing
+with open(output_file, "w") as f:
+    # Write the header line
+    f.write("APPLICATION,SUB_APPLICATION,PARENT_FOLDER,JOBNAME,VARIABLE_NAME,VARIABLE_VALUE\n")
+    # Loop through all the jobs in the folder
+    for job in root.find("FOLDER").findall("JOB"):
+        # Get the application, sub_application, parent_folder and jobname attributes
+        application = job.get("APPLICATION")
+        sub_application = job.get("SUB_APPLICATION")
+        parent_folder = job.get("PARENT_FOLDER")
+        jobname = job.get("JOBNAME")
+        # Loop through all the variables in the job
+        for variable in job.findall("VARIABLE"):
+            # Get the variable name and value attributes
+            variable_name = variable.get("NAME")
+            variable_value = variable.get("VALUE")
+            # Write a line with the information
+            f.write(f"{application},{sub_application},{parent_folder},{jobname},{variable_name},{variable_value}\n")
+
+# Close the output file
+f.close()
+
+# Print a message to indicate success
+print(f"Results saved to {output_file}")
+```
