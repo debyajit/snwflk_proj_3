b0089722cc5a5816e0f83f6b84a044875e0db1e9
Deb <39284636+debyajit@users.noreply.github.com>
2023-05-14 20:24:29 -0400

    Update dsreport.py

--- dsreport.py
+++ dsreport.py
@@ -1,69 +1,60 @@
 import xml.etree.ElementTree as ET
-import yaml
 
-# Parse the input XML file
-tree = ET.parse('input.xml')
-root = tree.getroot()
+def main():
+    # Get the input XML file name
+    xml_file_name = input("Enter the input XML file name: ")
 
-# Extract the required information from the XML
-job_name = root.attrib['Name']
-job_desc = root.attrib['Desc']
-job_status = root.attrib['Status']
-job_wave_no = root.attrib['WaveNo']
-job_start_date_time = root.attrib['StartDateTime']
-job_end_date_time = root.attrib['EndDateTime']
-job_elapsed_time = root.attrib['ElapsedTime']
-job_elapsed_secs = root.attrib['ElapsedSecs']
+    # Read the XML file
+    tree = ET.parse(xml_file_name)
 
-params = []
-for param in root.find('ParamSet').findall('Param'):
-    param_dict = {
-        'Name': param.attrib['Name'],
-        'Type': param.attrib['Type'],
-        'Desc': param.attrib['Desc'],
-        'Value': param.attrib['Value']
-    }
-    params.append(param_dict)
+    # Get the root of the XML tree
+    root = tree.getroot()
 
-stages = []
-for stage in root.find('ComponentSet').findall('Stage'):
-    stage_dict = {
-        'Name': stage.attrib['Name'],
-        'StageStatus': stage.attrib['StageStatus'],
-        'StageType': stage.attrib['StageType'],
-        'Desc': stage.attrib['Desc'],
-        'StartDateTime': stage.attrib['StartDateTime'],
-        'EndDateTime': stage.attrib['EndDateTime'],
-        'ElapsedTime': stage.attrib['ElapsedTime'],
-        'ElapsedSecs': stage.attrib['ElapsedSecs'],
-        'OutputLinks': []
-    }
-    for link in stage.find('OutputLinks').findall('Link'):
-        link_dict = {
-            'Name': link.attrib['Name'],
-            'LinkType': link.attrib['LinkType'],
-            'Desc': link.attrib['Desc'],
-            'Stage': link.attrib['Stage']
-        }
-        stage_dict['OutputLinks'].append(link_dict)
-    stages.append(stage_dict)
+    # Create a YAML file
+    yml_file = open("output.yml", "w")
 
-# Create a dictionary for the output YAML file
-output_dict = {
-    'Job': {
-        'Name': job_name,
-        'Desc': job_desc,
-        'Status': job_status,
-        'WaveNo': job_wave_no,
-        'StartDateTime': job_start_date_time,
-        'EndDateTime': job_end_date_time,
-        'ElapsedTime': job_elapsed_time,
-        'ElapsedSecs': job_elapsed_secs,
-        'ParamSet': params,
-        'ComponentSet': stages
-    }
-}
+    # Write the job details to the YAML file
+    yml_file.write("job:\n")
+    yml_file.write("  name: " + root.attrib["Name"] + "\n")
+    yml_file.write("  desc: " + root.attrib["Desc"] + "\n")
+    yml_file.write("  status: " + root.attrib["Status"] + "\n")
+    yml_file.write("  waveNo: " + root.attrib["WaveNo"] + "\n")
+    yml_file.write("  startDateTime: " + root.attrib["StartDateTime"] + "\n")
+    yml_file.write("  endDateTime: " + root.attrib["EndDateTime"] + "\n")
+    yml_file.write("  elapsedTime: " + root.attrib["ElapsedTime"] + "\n")
+    yml_file.write("  elapsedSecs: " + root.attrib["ElapsedSecs"] + "\n")
 
-# Write the output YAML file
-with open('output.yml', 'w') as file:
-    yaml.dump(output_dict, file)
+    # Write the ParamSet details to the YAML file
+    yml_file.write("paramSet:\n")
+    for param in root.findall("ParamSet/Param"):
+        yml_file.write("  name: " + param.attrib["Name"] + "\n")
+        yml_file.write("  type: " + param.attrib["Type"] + "\n")
+        yml_file.write("  desc: " + param.attrib["Desc"] + "\n")
+        yml_file.write("  value: " + param.attrib["Value"] + "\n")
+
+    # Write the Stage details to the YAML file
+    yml_file.write("stages:\n")
+    for stage in root.findall("ComponentSet/Stage"):
+        yml_file.write("  name: " + stage.attrib["Name"] + "\n")
+        yml_file.write("  stageStatus: " + stage.attrib["StageStatus"] + "\n")
+        yml_file.write("  stageType: " + stage.attrib["StageType"] + "\n")
+        yml_file.write("  desc: " + stage.attrib["Desc"] + "\n")
+        yml_file.write("  startDateTime: " + stage.attrib["StartDateTime"] + "\n")
+        yml_file.write("  endDateTime: " + stage.attrib["EndDateTime"] + "\n")
+        yml_file.write("  elapsedTime: " + stage.attrib["ElapsedTime"] + "\n")
+        yml_file.write("  elapsedSecs: " + stage.attrib["ElapsedSecs"] + "\n")
+
+        # Write the InputLinks details to the YAML file
+        yml_file.write("  inputLinks:\n")
+        for input_link in stage.findall("InputLinks/Link"):
+            yml_file.write("    name: " + input_link.attrib["Name"] + "\n")
+            yml_file.write("    linkType: " + input_link.attrib["LinkType"] + "\n")
+            yml_file.write("    desc: " + input_link.attrib["Desc"] + "\n")
+            yml_file.write("    stage: " + input_link.attrib["Stage"] + "\n")
+
+        # Write the OutputLinks details to the YAML file
+        yml_file.write("  outputLinks:\n")
+        for output_link in stage.findall("OutputLinks/Link"):
+            yml_file.write("    name: " + output_link.attrib["Name"] + "\n")
+            yml_file.write("    linkType: " + output_link.attrib["LinkType"] + "\n")
+            yml
