a49e635b171f4f09fa2f22e1fd7f36043fc1e92b
Deb <39284636+debyajit@users.noreply.github.com>
2023-05-14 20:43:12 -0400

    Update dsreport.py

--- dsreport.py
+++ dsreport.py
@@ -1,37 +1,30 @@
 import xml.etree.ElementTree as ET
 import yaml
 
-# Parse XML file
+# Read input XML file
 tree = ET.parse('input.xml')
 root = tree.getroot()
 
-# Create dictionary to store job details
-job_details = {}
-job_details['Job'] = {}
-job_details['Job']['Name'] = root.attrib['Name']
-job_details['Job']['Desc'] = root.attrib['Desc']
-job_details['Job']['Status'] = root.attrib['Status']
-job_details['Job']['WaveNo'] = root.attrib['WaveNo']
-job_details['Job']['StartDateTime'] = root.attrib['StartDateTime']
-job_details['Job']['EndDateTime'] = root.attrib['EndDateTime']
-job_details['Job']['ElapsedTime'] = root.attrib['ElapsedTime']
-job_details['Job']['ElapsedSecs'] = root.attrib['ElapsedSecs']
-
-# Create list to store parameter details
-param_list = []
-for param in root.findall("./ParamSet/Param"):
+# Parse XML data and create Python dictionary
+job_dict = {}
+job_dict['Job'] = {}
+job_dict['Job']['Name'] = root.attrib['Name']
+job_dict['Job']['Desc'] = root.attrib['Desc']
+job_dict['Job']['Status'] = root.attrib['Status']
+job_dict['Job']['WaveNo'] = root.attrib['WaveNo']
+job_dict['Job']['StartDateTime'] = root.attrib['StartDateTime']
+job_dict['Job']['EndDateTime'] = root.attrib['EndDateTime']
+job_dict['Job']['ElapsedTime'] = root.attrib['ElapsedTime']
+job_dict['Job']['ElapsedSecs'] = root.attrib['ElapsedSecs']
+job_dict['Job']['Params'] = {}
+for param in root.findall('.//Param'):
     param_dict = {}
-    param_dict['Name'] = param.attrib['Name']
     param_dict['Type'] = param.attrib['Type']
     param_dict['Desc'] = param.attrib['Desc']
     param_dict['Value'] = param.attrib['Value']
-    param_list.append(param_dict)
-
-job_details['ParamSet'] = param_list
-
-# Create list to store stage details
-stage_list = []
-for stage in root.findall("./ComponentSet/Stage"):
+    job_dict['Job']['Params'][param.attrib['Name']] = param_dict
+job_dict['Job']['Stages'] = {}
+for stage in root.findall('.//Stage'):
     stage_dict = {}
     stage_dict['Name'] = stage.attrib['Name']
     stage_dict['StageStatus'] = stage.attrib['StageStatus']
@@ -41,35 +34,33 @@ for stage in root.findall("./ComponentSet/Stage"):
     stage_dict['EndDateTime'] = stage.attrib['EndDateTime']
     stage_dict['ElapsedTime'] = stage.attrib['ElapsedTime']
     stage_dict['ElapsedSecs'] = stage.attrib['ElapsedSecs']
-    
-    # Create list to store input link details
-    input_links = []
-    for link in stage.findall("./InputLinks/Link"):
-        link_dict = {}
-        link_dict['Name'] = link.attrib['Name']
-        link_dict['LinkType'] = link.attrib['LinkType']
-        link_dict['Desc'] = link.attrib['Desc']
-        link_dict['Stage'] = link.attrib['Stage']
-        input_links.append(link_dict)
-    
-    stage_dict['InputLinks'] = input_links
-    
-    # Create list to store output link details
-    output_links = []
-    for link in stage.findall("./OutputLinks/Link"):
-        link_dict = {}
-        link_dict['Name'] = link.attrib['Name']
-        link_dict['LinkType'] = link.attrib['LinkType']
-        link_dict['Desc'] = link.attrib['Desc']
-        link_dict['Stage'] = link.attrib['Stage']
-        output_links.append(link_dict)
-    
-    stage_dict['OutputLinks'] = output_links
-    
-    stage_list.append(stage_dict)
+    stage_dict['InputLinks'] = {}
+    for input_link in stage.findall('.//InputLinks/Link'):
+        input_link_dict = {}
+        input_link_dict['LinkType'] = input_link.attrib['LinkType']
+        input_link_dict['Desc'] = input_link.attrib['Desc']
+        input_link_dict['Stage'] = input_link.attrib['Stage']
+        stage_dict['InputLinks'][input_link.attrib['Name']] = input_link_dict
+    stage_dict['OutputLinks'] = {}
+    for output_link in stage.findall('.//OutputLinks/Link'):
+        output_link_dict = {}
+        output_link_dict['LinkType'] = output_link.attrib['LinkType']
+        output_link_dict['Desc'] = output_link.attrib['Desc']
+        output_link_dict['Stage'] = output_link.attrib['Stage']
+        stage_dict['OutputLinks'][output_link.attrib['Name']] = output_link_dict
+    stage_dict['Instances'] = {}
+    for instance in stage.findall('.//InstanceSet/Instance'):
+        instance_dict = {}
+        instance_dict['CPU'] = instance.attrib['CPU']
+        instance_dict['PID'] = instance.attrib['PID']
+        instance_dict['InputLinks'] = {}
+        for input_link in instance.findall('.//Link'):
+            input_link_dict = {}
+            input_link_dict['RowCount'] = input_link.attrib['RowCount']
+            instance_dict['InputLinks'][input_link.attrib['Name']] = input_link_dict
+        stage_dict['Instances'][instance.attrib['Id']] = instance_dict
+    job_dict['Job']['Stages'][stage.attrib['Name']] = stage_dict
 
-job_details['ComponentSet'] = stage_list
-
-# Output YAML file
+# Write Python dictionary to output YAML file
 with open('output.yml', 'w') as outfile:
-    yaml.dump(job_details, outfile, default_flow_style=False)
+    yaml.dump(job_dict, outfile, default_flow_style=False)
