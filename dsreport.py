9b1bd814697644fd829ac351f176908529de2637
Deb <39284636+debyajit@users.noreply.github.com>
2023-05-17 23:02:22 -0400

    Update dsreport.py

--- dsreport.py
+++ dsreport.py
@@ -1,65 +1,48 @@
-import xml.etree.ElementTree as ET
-import yaml
+import xmltodict
 
-# Load XML file
-tree = ET.parse('input.xml')
-root = tree.getroot()
+with open("input.xml") as f:
+  xml_data = f.read()
+  dict_data = xmltodict.parse(xml_data)
+  # dict_data is a dictionary that contains the XML data
 
-# Initialize YAML data
-yaml_data = {}
+with open("reference.txt") as f:
+  ref_data = f.read()
+  ref_list = ref_data.split("~")
+  # ref_list is a list that contains the fields from the reference file
 
-# Parse job information
-job_info = {}
-job_info['Name'] = root.attrib['Name']
-job_info['Desc'] = root.attrib['Desc']
-job_info['Status'] = root.attrib['Status']
-job_info['WaveNo'] = root.attrib['WaveNo']
-job_info['StartDateTime'] = root.attrib['StartDateTime']
-job_info['EndDateTime'] = root.attrib['EndDateTime']
-job_info['ElapsedTime'] = root.attrib['ElapsedTime']
-job_info['ElapsedSecs'] = root.attrib['ElapsedSecs']
-yaml_data['Job'] = job_info
+OP1 = {} # initialize an empty dictionary
+for job in dict_data["DEFTABLE"]["FOLDER"]["JOB"]: # loop over each job in the input file
+  jobname = job["@JOBNAME"] # get the job name
+  parent_folder = job["@PARENT_FOLDER"] # get the parent folder
+  OP1[jobname] = {} # initialize a sub-dictionary for each job name
+  OP1[jobname]["PARENT_FOLDER"] = parent_folder # store the parent folder
+  for var in job["VARIABLE"]: # loop over each variable in the job
+    var_name = var["@NAME"] # get the variable name
+    var_value = var["@VALUE"] # get the variable value
+    OP1[jobname][var_name] = var_value # store the variable name and value
+    if var_value == ref_list[1]: # check if the variable value matches the second field from the reference file
+      OP1[jobname]["REF_FIELD_3"] = ref_list[2] # add the third field from the reference file
+      OP1[jobname]["REF_FIELD_4"] = ref_list[3] # add the fourth field from the reference file
+    else:
+      OP1[jobname]["REF_FIELD_3"] = "no match" # if no match, populate with "no match"
+      OP1[jobname]["REF_FIELD_4"] = "no match" # if no match, populate with "no match"
 
-# Parse stage information
-stage_list = []
-for component in root.findall('ComponentSet/Stage'):
-    stage_info = {}
-    stage_info['Name'] = component.attrib['Name']
-    stage_info['StageStatus'] = component.attrib['StageStatus']
-    stage_info['StageType'] = component.attrib['StageType']
-    stage_info['Desc'] = component.attrib['Desc']
-    stage_info['StartDateTime'] = component.attrib['StartDateTime']
-    stage_info['EndDateTime'] = component.attrib['EndDateTime']
-    stage_info['ElapsedTime'] = component.attrib['ElapsedTime']
-    stage_info['ElapsedSecs'] = component.attrib['ElapsedSecs']
+        for jobname in OP1: # loop over each job name in OP1
+  if "." in ref_list[1]: # check if there is a "." character in the second field from the reference file
+    OP1[jobname]["DERIVED_FIELD"] = "A" # populate with "A"
 
-    # Parse input link information
-    input_link_list = []
-    for input_link in component.findall('InputLinks/Link'):
-        if input_link.get('LinkType') == '1':
-            input_link_info = {}
-            input_link_info['Name'] = input_link.attrib['Name']
-            input_link_info['Desc'] = input_link.attrib['Desc']
-            input_link_info['Stage'] = input_link.attrib['Stage']
-            input_link_list.append(input_link_info)
-    stage_info['InputLinks'] = input_link_list
-
-    # Parse output link information
-    output_link_list = []
-    for output_link in component.findall('OutputLinks/Link'):
-        output_link_info = {}
-        output_link_info['Name'] = output_link.attrib['Name']
-        output_link_info['Desc'] = output_link.attrib['Desc']
-        output_link_info['Stage'] = output_link.attrib['Stage']
-        output_link_list.append(output_link_info)
-    stage_info['OutputLinks'] = output_link_list
-
-    # Append stage information to list
-    stage_list.append(stage_info)
-
-# Add stage information to YAML data
-yaml_data['Stage'] = stage_list
-
-# Output YAML data to file
-with open('output.yml', 'w') as f:
-    yaml.dump(yaml_data, f, default_flow_style=False)
+    with open("output.txt", "w") as f: # open an output text file for writing
+  for jobname in OP1: # loop over each job name in OP1
+    parent_folder = OP1[jobname]["PARENT_FOLDER"] # get the parent folder
+    f.write(parent_folder + ",") # write it to the output file with a comma
+    f.write(jobname + ",") # write the job name to the output file with a comma
+    for var_name in OP1[jobname]: # loop over each variable name in OP1[jobname]
+      if var_name not in ["PARENT_FOLDER", "REF_FIELD_3", "REF_FIELD_4", "DERIVED_FIELD"]: # exclude these fields as they are already written or will be written later
+        var_value = OP1[jobname][var_name] # get the variable value
+        f.write(var_name + "," + var_value + ",") # write them to the output file with commas
+    ref_field_3 = OP1[jobname]["REF_FIELD_3"] # get the third field from the reference file
+    f.write(ref_field_3 + ",") # write it to the output file with a comma
+    ref_field_4 = OP1[jobname]["REF_FIELD_4"] # get the fourth field from the reference file
+    f.write(ref_field_4 + ",") # write it to the output file with a comma
+    derived_field = OP1[jobname]["DERIVED_FIELD"] # get the derived field 
+    f.write(derived_field + "\n") # write it to the output file with a newline character
