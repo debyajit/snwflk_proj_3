7bb9352d73b33b065b59eeff7836fc5917ea50a1
Deb <39284636+debyajit@users.noreply.github.com>
2023-05-18 15:03:48 -0400

    Update dsreport.py

--- dsreport.py
+++ dsreport.py
@@ -1,72 +1,33 @@
-import xml.etree.ElementTree as ET
-import csv
+import os
 
-# Read the input XML file
-xml_file = open("input.xml", "r")
-xml_tree = ET.parse(xml_file)
+dc_1 = {
+    'ABC': {
+        'PARENT_FOLDER': 'FLD',
+        'ref_1': 'DS',
+        'ref_2': 'dsjb1'
+    },
+    'DEF': {
+        'PARENT_FOLDER': 'FLD',
+        'ref_1': 'FP',
+        'ref_2': ''
+    }
+}
 
-# Read the reference text file
-reference_file = open("reference.txt", "r")
-reference_reader = csv.reader(reference_file, delimiter="~")
+for key, value in dc_1.items():
+    ref_2 = value['ref_2']
+    if ref_2.startswith('ds'):
+        command = f"dsjob {ref_2}"
+        output_file = f"output_{key}.txt"
 
-# Create a dictionary to store the output data
-op1 = {}
+        # Change directory to /Scripts
+        os.chdir("/Scripts")
 
-# Iterate over the XML tree
-for job in xml_tree.findall(".//JOB"):
+        # Run the command and capture the output in a file
+        os.system(f"{command} > {output_file}")
 
-    # Get the parent folder, job name, and variable name
-    parent_folder = job.get("PARENT_FOLDER")
-    job_name = job.get("JOBNAME")
+        # Write dc_1 row to the output file
+        with open(output_file, 'a') as f:
+            f.write(f"\ndc_1 row: {key}: {value}")
 
-    # Check if the variable element exists
-    if job.find("VARIABLE") is not None:
-        variable_name = job.find("VARIABLE").get("NAME")
-    else:
-        variable_name = None
-
-    # Get the corresponding data from the reference file
-    for row in reference_reader:
-        if variable_name is not None and row[0] == variable_name:
-            if row:
-                op1[job_name] = {
-                    "PARENT_FOLDER": parent_folder,
-                    "JOBNAME": job_name,
-                    "VARIABLE_NAME": variable_name,
-                    "VARIABLE_VALUE": row[1] if row else "no match",
-                    "REFERENCE_1": row[2],
-                    "REFERENCE_2": row[3],
-                }
-                break
-
-    # Check if the variable was found in the reference file
-    if variable_name is not None and variable_name not in op1:
-        op1[job_name] = {
-            "PARENT_FOLDER": parent_folder,
-            "JOBNAME": job_name,
-            "VARIABLE_NAME": variable_name,
-            "VARIABLE_VALUE": "no match",
-            "REFERENCE_1": "no match",
-            "REFERENCE_2": "no match",
-        }
-
-    # Check if the reference file has a . character in the second field
-    if "." in op1.get(job_name, {}).get("REFERENCE_1", ""):
-        op1[job_name]["HAS_DOT"] = "A"
-    else:
-        op1[job_name]["HAS_DOT"] = "B"
-
-# Iterate over the output dictionary
-for job in op1:
-
-    # Go to the /proj/scripts directory
-    os.chdir("/proj/scripts")
-
-    # Execute the dsreport command
-    dsreport_command = "dsreport " + op1[job]["REFERENCE_2"]
-    dsreport_output = subprocess.check_output(dsreport_command, shell=True)
-
-    # Save the dsreport output to a file
-    with open("dsreport_output.xml", "wb") as f:
-        f.write(dsreport_output)
-
+        # Change back to the previous directory
+        os.chdir("..")
