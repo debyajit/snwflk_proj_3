21cb03c19446f0f109b9dc2bc3b433ecf037e305
Deb <39284636+debyajit@users.noreply.github.com>
2023-06-02 18:50:08 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,17 +1,6 @@
 
 awk -F "~" '{ if (NF < 68) { getline nextLine; $0 = $0 nextLine; } print }' input.csv > output.csv
 
+awk '!/^2023/ { printf "%s", prev; prev = $0; next } { print prev; prev = $0 } END { print prev }' input.txt > output.txt
 
-awk -F "~" '{
-  if (NF < 68) {
-    getline nextLine;
-    if (NF + split(nextLine, fields, "~") < 68) {
-      $0 = $0 nextLine;
-    } else {
-      print;
-      $0 = nextLine;
-    }
-  }
-  print
-}' input.csv > output.csv
 
