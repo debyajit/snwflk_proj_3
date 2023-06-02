8e0984fd408b21a4b3b1cdaddc3f619319eae70c
Deb <39284636+debyajit@users.noreply.github.com>
2023-06-02 18:21:46 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,1 +1,3 @@
 awk 'BEGIN{ RS=""; FS="\n"; OFS=""; ORS="\n" } { for (i=1; i<=NF; i++) { if ($i != "") { printf("%s~", $i) } } print "" }' input.txt > output.txt
+
+awk 'BEGIN{ RS="\n\n"; FS="\n"; ORS="" } { for (i=1; i<=NF; i++) { if (i == 1) printf("%s", $i); else printf("~%s", $i); } print "" }' input.txt > output.txt
