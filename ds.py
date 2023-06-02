b3a1a5077f55ac8a39e0b8b77bbaf9ecade1543a
Deb <39284636+debyajit@users.noreply.github.com>
2023-06-02 18:17:32 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,1 +1,1 @@
-sed '/^\s*$/d' file.txt | tr -s '\n' -t '~' > new_file.txt
+awk 'BEGIN{ RS=""; FS="\n"; OFS=""; ORS="\n" } { for (i=1; i<=NF; i++) { if ($i != "") { printf("%s~", $i) } } print "" }' input.txt > output.txt
