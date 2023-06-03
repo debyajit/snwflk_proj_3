4b0eb89672c8d951f9d4c9f54d18ebc262218985
Deb <39284636+debyajit@users.noreply.github.com>
2023-06-03 10:47:04 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -3,6 +3,6 @@ awk -F "~" '{ if (NF < 68) { getline nextLine; $0 = $0 nextLine; } print }' inpu
 
 awk '!/^2023/ { printf "%s", prev; prev = $0; next } { print prev; prev = $0 } END { print prev }' input.txt > output.txt
 
-awk '!/^2023/ { printf "%s", prev; prev = $0; next } { print prev; prev = $0 } END { if (prev != "") print prev }' input.txt > output.txt
+sed -e :a -e '$!N; /^2023/!s/\n/ /; ta' -e 'P;D' input.txt > output.txt
 
 
