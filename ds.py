9bc0aadf3971d6a8480567bd0d0faa7500f5c987
Deb <39284636+debyajit@users.noreply.github.com>
2023-06-03 11:22:46 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -3,33 +3,31 @@ awk -F "~" '{ if (NF < 68) { getline nextLine; $0 = $0 nextLine; } print }' inpu
 
 awk '!/^2023/ { printf "%s", prev; prev = $0; next } { print prev; prev = $0 } END { print prev }' input.txt > output.txt
 
+
 #!/bin/bash
 
+# Input and output file paths
 input_file="input.txt"
 output_file="output.txt"
-delimiter="~"
-line_count=0
-current_line=""
-previous_line=""
 
-while IFS= read -r line
-do
-    if [[ $line_count -eq 0 ]]; then
-        previous_line=$line
-    else
-        if [[ $line =~ ^2023 ]]; then
-            echo "$previous_line" >> "$output_file"
-            previous_line=$line
-        else
-            previous_line+="$(printf "${delimiter}%s" "$line")"
-        fi
+# Read the input file line by line
+while IFS= read -r line; do
+  # Check if the line starts with "2023"
+  if [[ $line == 2023* ]]; then
+    # Output the line as is
+    echo "$line" >> "$output_file"
+  else
+    # Concatenate the line with the previous line
+    concatenated_line=$(echo -e "${concatenated_line}${line}")
+    # Check if the concatenated line contains 68 delimiters
+    delimiter_count=$(awk -F "~" '{print NF-1}' <<< "$concatenated_line")
+    if [[ $delimiter_count -eq 68 ]]; then
+      # Output the concatenated line with the end of line character
+      echo "${concatenated_line}$" >> "$output_file"
+      # Reset the concatenated line
+      concatenated_line=""
     fi
-
-    line_count=$((line_count + 1))
+  fi
 done < "$input_file"
-
-# Add the last line to the output file
-echo "$previous_line" >> "$output_file"
-
 
 
