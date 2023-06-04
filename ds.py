dee39ee891a9928583dd05cee8a61dc9fac43f22
Deb <39284636+debyajit@users.noreply.github.com>
2023-06-03 22:12:00 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-# Input and output file paths
-input_file="input.txt"
-output_file="output.txt"
+# Check if the file name is provided as an argument
+if [ $# -eq 0 ]; then
+  echo "Please provide the file name as an argument."
+  exit 1
+fi
 
-# Read the input file line by line
-while IFS= read -r line; do
-  # Check if the line starts with "2023"
-  if [[ $line == 2023* ]]; then
-    # Output the line as is
-    echo "$line" >> "$output_file"
-  else
-    # Concatenate the line with the previous line
-    concatenated_line=$(echo -e "${concatenated_line}~${line}")
-    # Check if the concatenated line contains 68 delimiters
-    delimiter_count=$(awk -F "~" '{print NF-1}' <<< "$concatenated_line")
-    if [[ $delimiter_count -eq 68 ]]; then
-      # Replace the first "~" with an empty space
-      concatenated_line=${concatenated_line/#~}
-      # Output the concatenated line with the end of line character
-      echo "${concatenated_line}$" >> "$output_file"
-      # Reset the concatenated line
-      concatenated_line=""
-    fi
+# Store the file name from the argument
+file="$1"
+
+# Create a new file name for the output
+output_file="${file}_output"
+
+# Read the file line by line and find the line number with a blank line
+blank_line_num=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  ((blank_line_num++))
+  if [[ -z $line ]]; then
+    break
   fi
-done < "$input_file"
+done < "$file"
+
+# Delete the line with line number = blank_line_num
+sed -i "${blank_line_num}d" "$file"
+
+# Concatenate the lines with line numbers blank_line_num-1 and blank_line_num
+if [ "$blank_line_num" -gt 1 ]; then
+  sed -i "${blank_line_num}s/$/$(sed -n "${blank_line_num-1}p" "$file")/" "$file"
+fi
+
+# Write the updated file to the output file
+cp "$file" "$output_file"
+
+# Print the success message
+echo "Output written to $output_file"
