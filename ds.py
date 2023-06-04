71ff1afc815ac25321e2328e9e818c22b2448bd4
Deb <39284636+debyajit@users.noreply.github.com>
2023-06-03 22:27:38 -0400

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-# Check if the file name is provided as an argument
-if [ $# -eq 0 ]; then
-  echo "Please provide the file name as an argument."
-  exit 1
-fi
+input_file="$1"
+output_file="output.csv"
 
-# Store the file name from the argument
-file="$1"
+# Delete blank lines from input file
+sed '/^$/d' "$input_file" > "$output_file"
 
-# Create a new file name for the output
-output_file="${file}_output"
+# Concatenate lines that don't start with "2023" with previous line
+awk -F "~" 'BEGIN {OFS="~"; prev=""} { if ($1 ~ /^2023/) { if (prev != "") print prev; prev=$0; } else { prev = prev $0 } } END { print prev }' "$output_file" > "$output_file.tmp"
 
-# Read the file line by line and find the line number with a blank line
-blank_line_num=0
-while IFS='' read -r line || [[ -n "$line" ]]; do
-  ((blank_line_num++))
-  if [[ -z $line ]]; then
-    break
-  fi
-done < "$file"
+# Append any remaining concatenated line to the output file
+cat "$output_file.tmp" >> "$output_file"
 
-# Delete the line with line number = blank_line_num
-sed -i "${blank_line_num}d" "$file"
-
-# Concatenate the lines with line numbers blank_line_num-1 and blank_line_num
-if [ "$blank_line_num" -gt 1 ]; then
-  awk -v line_num=$((blank_line_num-1)) 'NR==line_num{line=$0} NR==line_num+1{$0=$0 line} NR!=line_num{print}' "$file" > "$output_file"
-else
-  cp "$file" "$output_file"
-fi
-
-# Print the success message
-echo "Output written to $output_file"
+# Remove temporary file
+rm "$output_file.tmp"
